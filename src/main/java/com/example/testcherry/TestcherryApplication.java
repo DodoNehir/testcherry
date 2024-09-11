1843a8e5bc9dd92780da1e81b119961a1792716e
DodoNehir <rojalaurant@gmail.com>
2024-09-11 23:29:58 +0900

    [init] initial commit

---
+++ src/main/java/com/example/testcherry/TestcherryApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcherry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcherryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestcherryApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/testcherry/TestcherryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.testcherry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcherryApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+
+}
