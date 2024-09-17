81f330086d23a0c179088175fd7046b18a6832ac
DodoNehir <rojalaurant@gmail.com>
2024-09-17 17:00:34 +0900

    [feat] use Thymeleaf

--- src/main/java/com/example/testcherry/controller/CherryController.java
+++ src/main/java/com/example/testcherry/controller/CherryController.java
@@ -1,9 +1,14 @@
 package com.example.testcherry.controller;
 
 import com.example.testcherry.service.CherryService;
+import java.lang.reflect.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class CherryController {
   private CherryService cherryService;
 
@@ -11,5 +16,20 @@ public class CherryController {
     this.cherryService = cherryService;
   }
 
+  @GetMapping("/members/new")
+  public String createForm() {
+    return "members/createMemberForm.html";
+  }
+
+  @PostMapping("/members/new")
+  public String create(MemberForm form) {
+    Member member = new Member();
+    member.setName(form.getName());
+
+    cherryService.join(member);
+
+    return "redirect:/";
+
+  }
 
 }
