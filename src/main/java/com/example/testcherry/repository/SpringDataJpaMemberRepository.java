a7130ffd545ea21f7a0553eac11ca8551634c450
DodoNehir <rojalaurant@gmail.com>
2024-09-20 00:03:40 +0900

    [fix] build error

--- src/main/java/com/example/testcherry/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/com/example/testcherry/repository/SpringDataJpaMemberRepository.java
@@ -4,9 +4,9 @@ import com.example.testcherry.domain.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long> {
 
-  @Override
+
   Optional<Member> findByName(String name);
 
 }
