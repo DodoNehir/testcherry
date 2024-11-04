655ca279329c6cc95606f6326234bc752ce647d7
DodoNehir <rojalaurant@gmail.com>
2024-11-05 07:09:25 +0900

    [feat] data.sql 사용

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,19 @@
 -- members
-INSERT INTO MEMBERS
-VALUES (1, 'Alice', 'Seoul', '010-0000-0000', true);
-INSERT INTO MEMBERS
-VALUES (2, 'Bob', 'TT', '531-111-1111', true);
+INSERT INTO MEMBERS(member_id, username, password, address, phone_number, active)
+VALUES (1, 'qwerty', '$2a$10$NKps6HDOWCQ3AO6S6tTXoO9epU6J34ZLxnxlimEWGLcpJhJZQXdSy', 'Seoul',
+        '010-1111-1111', true);
+INSERT INTO MEMBERS(member_id, username, password, address, phone_number, active)
+VALUES (2, 'asdf', '$2a$10$DNKX.ZXw6DqmKRTOVDW1CenrWZ7gMHFVB0/sacEBrP28N3IHV8tz6', 'Seoul',
+        '010-2222-2222', true);
 
 -- products
-INSERT INTO PRODUCTS
-VALUES (1, '아이스 아메리카노', '필수품', 50);
-INSERT INTO PRODUCTS
-VALUES (2, '참치', '', 50);
-INSERT INTO PRODUCTS
-VALUES (3, '김치', '유산균', 50);
-INSERT INTO PRODUCTS
-VALUES (4, '개복치', '해산물', 50);
+INSERT INTO PRODUCTS(product_id, name, description, quantity)
+VALUES (1, 'Red velvet cake', 'Android 11', 100);
+INSERT INTO PRODUCTS(product_id, name, description, quantity)
+VALUES (2, 'Snow cone', 'Android 12', 100);
+INSERT INTO PRODUCTS(product_id, name, description, quantity)
+VALUES (3, 'Tiramisu', 'Android 13', 100);
+INSERT INTO PRODUCTS(product_id, name, description, quantity)
+VALUES (4, 'Upside down cake', 'Android 14', 100);
+INSERT INTO PRODUCTS(product_id, name, description, quantity)
+VALUES (5, 'Vanilla ice cream', 'Android 15', 100);
