7c35b1922832a029c92603abba22de832e59687c
DodoNehir <rojalaurant@gmail.com>
2024-12-18 01:32:45 +0900

    [feat] product controller 권한 테스트 작성

---
+++ src/test/java/com/example/testcherry/controller/ProductControllerTest.java
@@ -0,0 +1,159 @@
+package com.example.testcherry.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.testcherry.model.entity.Product;
+import com.example.testcherry.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class ProductControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ProductRepository productRepository;
+
+  private Product product;
+
+
+  @BeforeEach
+  void setUp() {
+    product = new Product("ABCD", "HHA", 50);
+    product = productRepository.save(product);
+  }
+
+  // CREATE
+  @Test
+  @WithMockUser
+  void registerProductWithAnonymousRole() throws Exception {
+    mockMvc.perform(post("/products")
+            .contentType("application/json")
+            .content("{\n"
+                + "    \"name\": \"Apple\",\n"
+                + "    \"description\": \"Silicon\",\n"
+                + "    \"quantity\": \"100\"\n"
+                + "}"))
+        .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithMockUser(roles = "MEMBER")
+  void registerProductWithMemberRole() throws Exception {
+    mockMvc.perform(post("/products")
+            .contentType("application/json")
+            .content("{\n"
+                + "    \"name\": \"Apple\",\n"
+                + "    \"description\": \"Silicon\",\n"
+                + "    \"quantity\": \"100\"\n"
+                + "}"))
+        .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithMockUser(roles = "ADMIN")
+  void registerProductWithAdminRole() throws Exception {
+    mockMvc.perform(post("/products")
+            .contentType("application/json")
+            .content("{\n"
+                + "    \"name\": \"Apple\",\n"
+                + "    \"description\": \"Silicon\",\n"
+                + "    \"quantity\": \"100\"\n"
+                + "}"))
+        .andExpect(status().isOk());
+  }
+
+
+  // READ
+  @Test
+  @WithMockUser
+  void getAllProductsWithoutRole() throws Exception {
+    mockMvc.perform(get("/products/all"))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  @WithMockUser(roles = "MEMBER")
+  void getAllProductsWithMemberRole() throws Exception {
+    mockMvc.perform(get("/products/all"))
+        .andExpect(status().isOk());
+  }
+
+
+  // UPDATE
+  @Test
+  @WithMockUser
+  void updateProductWithAnonymousRole() throws Exception {
+    mockMvc.perform(patch("/products/" + product.getProductId())
+            .contentType("application/json")
+            .content("{\n"
+                + "    \"name\": \"Apple\",\n"
+                + "    \"description\": \"Silicon\",\n"
+                + "    \"quantity\": \"100\"\n"
+                + "}"))
+        .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithMockUser(roles = "MEMBER")
+  void updateProductWithMemberRole() throws Exception {
+    mockMvc.perform(patch("/products/" + product.getProductId())
+            .contentType("application/json")
+            .content("{\n"
+                + "    \"name\": \"Apple\",\n"
+                + "    \"description\": \"Silicon\",\n"
+                + "    \"quantity\": \"100\"\n"
+                + "}"))
+        .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithMockUser(roles = "ADMIN")
+  void updateProductWithAdminRole() throws Exception {
+    mockMvc.perform(patch("/products/" + product.getProductId())
+            .contentType("application/json")
+            .content("{\n"
+                + "    \"name\": \"Apple\",\n"
+                + "    \"description\": \"Silicon\",\n"
+                + "    \"quantity\": \"100\"\n"
+                + "}"))
+        .andExpect(status().isOk());
+  }
+
+
+  // DELETE
+  @Test
+  @WithMockUser
+  void deleteProductWithAnonymousRole() throws Exception {
+    mockMvc.perform(delete("/products/" + product.getProductId()))
+        .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithMockUser(roles = "MEMBER")
+  void deleteProductWithMemberRole() throws Exception {
+    mockMvc.perform(delete("/products/" + product.getProductId()))
+        .andExpect(status().isForbidden());
+  }
+
+  @Test
+  @WithMockUser(roles = "ADMIN")
+  void deleteProductWithAdminRole() throws Exception {
+    mockMvc.perform(delete("/products/" + product.getProductId()))
+        .andExpect(status().isOk());
+  }
+}
